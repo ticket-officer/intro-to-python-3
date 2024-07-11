8c2d52e4b5e21073adb008b6f6954cb066bab1e2
Ticket To Tech <admin@tickettotech.org>
2024-07-12 00:39:46 +0800

    Create hangman.py

---
+++ hangman/hangman.py
@@ -0,0 +1,137 @@
+import random
+
+def load_words():
+    with open("words.txt", "r") as file:
+        words = file.readlines()
+    return [word.strip() for word in words]
+
+def choose_word(words):
+    return random.choice(words)
+
+def display_hangman(lives):
+    stages = [
+        '''
+           -----
+           |   |
+           O   |
+          /|\\  |
+          / \\  |
+               |
+        ---------
+        ''',
+        '''
+           -----
+           |   |
+           O   |
+          /|\\  |
+          /    |
+               |
+        ---------
+        ''',
+        '''
+           -----
+           |   |
+           O   |
+          /|\\  |
+               |
+               |
+        ---------
+        ''',
+        '''
+           -----
+           |   |
+           O   |
+          /|   |
+               |
+               |
+        ---------
+        ''',
+        '''
+           -----
+           |   |
+           O   |
+           |   |
+               |
+               |
+        ---------
+        ''',
+        '''
+           -----
+           |   |
+           O   |
+               |
+               |
+               |
+        ---------
+        ''',
+        '''
+           -----
+           |   |
+               |
+               |
+               |
+               |
+        ---------
+        '''
+    ]
+    return stages[lives]
+
+def play_game(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    lives = 6
+
+    print("Let's play Hangman!")
+    print(display_hangman(lives))
+    print(word_completion)
+    print("\n")
+
+    while not guessed and lives > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                lives -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                lives -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Not a valid guess.")
+        print(display_hangman(lives))
+        print(word_completion)
+        print("\n")
+
+    if guessed:
+        print("Congratulations, you guessed the word! You win!")
+    else:
+        print("Sorry, you ran out of lives. The word was " + word + ". Maybe next time!")
+
+def main():
+    words = load_words()
+    word = choose_word(words)
+    play_game(word)
+
+if __name__ == "__main__":
+    main()
